d099cb3c3da2dc1718d5390b20201725fdebb3a7
Zhengfei Wang <wangzhengfei@pku.edu.cn>
2019-02-14 00:10:07 +0800

    WIP: complete build_network

--- a3c_agent.py
+++ a3c_agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+from pysc2.lib import actions
 
 from network import build_network
 import utils as U
@@ -31,7 +32,53 @@ class A3CAgent(object):
             )
 
             # build network
-            self.non_spatial_action, self.spatial_action, self.value = build_network()
+            network = build_network(self.non_spatial_ph, self.screen_ph, self.minimap_ph, len(actions.FUNCTIONS))
+            self.non_spatial_action, self.spatial_action, self.value = network
+
+            # placeholder for targets and masks
+            self.valid_non_spatial_action_ph = tf.placeholder(
+                tf.float32,
+                [None, len(actions.FUNCTIONS)],
+                name='valid_non_spatial_action'
+            )
+            self.sample_non_spatial_action_ph = tf.placeholder(
+                tf.float32,
+                [None, len(actions.FUNCTIONS)],
+                name='sample_non_spatial_action'
+            )
+            self.valid_spatial_action_ph = tf.placeholder(tf.float32, [None], name='valid_spatial_action')
+            self.sample_spatial_action_ph = tf.placeholder(
+                tf.float32,
+                [None, self.minimap_dimensions ** 2],
+                name='sample_spatial_action'
+            )
+            self.target_value_ph = tf.placeholder(tf.float32, [None], name='target_value')
+
+            # compute log probability
+            valid_non_spatial_action_prob = tf.reduce_sum(self.non_spatial_action * self.valid_non_spatial_action_ph, axis=1)
+            valid_non_spatial_action_prob = tf.clip_by_value(valid_non_spatial_action_prob, 1e-10, 1.)
+            non_spatial_action_prob = tf.reduce_sum(self.non_spatial_action * self.sample_non_spatial_action_ph, axis=1)
+            non_spatial_action_prob /= valid_non_spatial_action_prob
+            non_spatial_action_log_prob = tf.log(tf.clip_by_value(non_spatial_action_prob, 1e-10, 1.))
+            spatial_action_prob = tf.reduce_sum(self.spatial_action * self.sample_spatial_action_ph, axis=1)
+            spatial_action_log_prob = tf.log(tf.clip_by_value(spatial_action_prob, 1e-10, 1.))
+
+            # compute loss
+            action_log_prob = self.valid_spatial_action_ph * spatial_action_log_prob + non_spatial_action_log_prob
+            advantage = tf.stop_gradient(self.target_value_ph - self.value)
+            policy_loss = -tf.reduce_mean(action_log_prob * advantage)
+            value_loss = -tf.reduce_mean(self.value * advantage)
+            loss = policy_loss + value_loss
+
+            # optimizer
+            self.learning_rate_ph = tf.placeholder(tf.float32, None, name='learning_rate')
+            optimizer = tf.train.RMSPropOptimizer(self.learning_rate_ph, decay=0.99, epsilon=1e-10)
+            grads = optimizer.compute_gradients(loss)
+            clipped_grads = []
+            for grad, var in grads:
+                grad = tf.clip_by_value(grad, 10.0)
+                clipped_grads.append([grad, var])
+            self.train_op = optimizer.minimize(clipped_grads)
 
     def step(self, obs):
         screen = np.array(obs.observation.screen, dtype=np.float32)
--- network.py
+++ network.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def build_network(non_spatial, screen, minimap, screen_dimensions, minimap_dimensions):
+def build_network(non_spatial, screen, minimap, action_dimensions):
     non_spatial_fc = tf.contrib.layers.fully_connected(
         inputs=tf.contrib.layers.flatten(non_spatial),
         num_outputs=256,
@@ -39,14 +39,39 @@ def build_network(non_spatial, screen, minimap, screen_dimensions, minimap_dimen
         scope='minimap_conv2'
     )
 
+    spatial_action = tf.contrib.layers.conv2d(
+        inputs=tf.concat([screen_conv2, minimap_conv2], axis=3),
+        num_outputs=1,
+        kernel_size=1,
+        strides=1,
+        activation_fn=None,
+        scope='spatial_action'
+    )
+
     state_representation = tf.concat([
         tf.contrib.layers.flatten(screen_conv2),
         tf.contrib.layers.flatten(minimap_conv2),
         non_spatial_fc
     ], axis=1)
+    state_representation_fc = tf.contrib.layers.fully_connected(
+        inputs=state_representation,
+        num_outputs=256,
+        activation_fn=tf.nn.relu,
+        scope='state_representation_fc'
+    )
 
-    spatial_action = None
-    non_spatial_action = None
-    value = None
+    spatial_action = tf.nn.softmax(tf.contrib.layers.flatten(spatial_action))
+    non_spatial_action = tf.contrib.layers.fully_connected(
+        inputs=state_representation_fc,
+        num_outputs=action_dimensions,
+        activation_fn=tf.nn.softmax,
+        scope='non_spatial_action'
+    )
+    value = tf.reshape(tf.contrib.layers.fully_connected(
+        inputs=state_representation_fc,
+        num_outputs=1,
+        activation_fn=None,
+        scope='value'
+    ), -1)
 
     return non_spatial_action, spatial_action, value
