2b9decd14d66c6000a5d13a98527a31bc31a1098
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2019-02-23 01:57:09 +0800

    Optimized further
    remove .idea, transfer from argparser to absl.flags

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 import sys
 import threading
-from absl import flags
-import argparse
+from absl import app, flags
 import tensorflow as tf
 from pysc2.env import sc2_env, available_actions_printer
 
 from a3c_agent import A3CAgent
 from rollout import rollout
 
+
 DEVICE = ['/cpu:0']
 LOCK = threading.Lock()
 STEP = 0
 FLAGS = flags.FLAGS
+flags.DEFINE_string('map_name', 'MoveToBeacon', 'SC2LE mini-games map name')
+flags.DEFINE_integer('num_envs', 2, 'Number of environments')
+flags.DEFINE_integer('screen_resolution', 64, 'Resolution of the screen')
+flags.DEFINE_integer('minimap_resolution', 64, 'Resolution of the minimap')
+flags.DEFINE_integer('step_mul', 8, 'Number of game steps per agent step')
+flags.DEFINE_bool('render', False, 'Whether to render with pygame')
+flags.DEFINE_bool('train', True, 'Whether to train agents')
+flags.DEFINE_integer('max_steps', int(1e5), 'Total steps for training')
+flags.DEFINE_integer('episode_horizon', 60, 'Total steps for every agents')
+flags.DEFINE_integer('save_interval', 10, 'Number of steps between saving events')
+flags.DEFINE_float('learning_rate', 5e-4, 'Learning rate')
+flags.DEFINE_float('gamma', 0.99, 'Discounting factor')
+flags.DEFINE_string('log_dir', './logs', 'Logs output directory')
+FLAGS(sys.argv)
 
 
 def run_thread(agent, visualize):
     with sc2_env.SC2Env(
-        map_name=args.map_name,
+        map_name=FLAGS.map_name,
         agent_interface_format=[sc2_env.AgentInterfaceFormat(
             feature_dimensions=sc2_env.Dimensions(
-                screen=(args.screen_resolution, args.screen_resolution),
-                minimap=(args.minimap_resolution, args.minimap_resolution)
+                screen=(FLAGS.screen_resolution, FLAGS.screen_resolution),
+                minimap=(FLAGS.minimap_resolution, FLAGS.minimap_resolution)
             )
         )],
-        step_mul=args.step_mul,
+        step_mul=FLAGS.step_mul,
         visualize=visualize
     ) as env:
         env = available_actions_printer.AvailableActionsPrinter(env)
 
         replay_buffer = []
-        for trajectory, done in rollout([agent], env, args.max_steps):
-            if args.train:
+        for trajectory, done in rollout([agent], env, FLAGS.max_steps):
+            if FLAGS.train:
                 replay_buffer.append(trajectory)
                 if done:
                     step = 0
@@ -38,60 +52,41 @@ def run_thread(agent, visualize):
                         global STEP
                         STEP += 1
                         step = STEP
-                    learning_rate = args.learning_rate * (1 - 0.9 * step / args.max_steps)
-                    agent.update(replay_buffer, args.gamma, learning_rate, step)
+                    learning_rate = FLAGS.learning_rate * (1 - 0.9 * step / FLAGS.max_steps)
+                    agent.update(replay_buffer, FLAGS.gamma, learning_rate, step)
                     replay_buffer = []
 
-                    if step >= args.max_steps:
+                    if step >= FLAGS.max_steps:
                         break
 
 
 def main():
     agents = []
-    for i in range(args.num_envs):
-        agent = A3CAgent(args.screen_resolution, args.minimap_resolution, args.train)
+    for i in range(FLAGS.num_envs):
+        agent = A3CAgent(FLAGS.screen_resolution, FLAGS.minimap_resolution, FLAGS.train)
         agent.build_model(i > 0, DEVICE[0])
         agents.append(agent)
 
     config = tf.ConfigProto(allow_soft_placement=True)
     sess = tf.Session(config=config)
 
-    summary_writer = tf.summary.FileWriter(args.log_dir)
-    for i in range(args.num_envs):
+    summary_writer = tf.summary.FileWriter(FLAGS.log_dir)
+    for i in range(FLAGS.num_envs):
         agents[i].setup(sess, summary_writer)
 
     agent.initialize()
 
     threads = []
-    for i in range(args.num_envs - 1):
+    for i in range(FLAGS.num_envs - 1):
         t = threading.Thread(target=run_thread, args=(agents[i], False))
         threads.append(t)
         t.daemon = True
         t.start()
-    run_thread(agents[-1], args.render)
+    run_thread(agents[-1], FLAGS.render)
 
     for t in threads:
         t.join()
 
 
 if __name__ == '__main__':
-    FLAGS(sys.argv)
-
-    parser = argparse.ArgumentParser(description='StarCraft II mini-games reinforcement learning a3c agents.')
-    parser.add_argument('--map-name', type=str, default='MoveToBeacon', help='SC2LE mini-games map name')
-    parser.add_argument('--num-envs', type=int, default=1, help='Number of environments')
-    parser.add_argument('--screen-resolution', type=int, default=64, help='Resolution of the screen')
-    parser.add_argument('--minimap-resolution', type=int, default=64, help='Resolution of the minimap')
-    parser.add_argument('--step-mul', type=int, default=8, help='Number of game steps per agent step')
-    parser.add_argument('--render', action='store_true', default=False, help='Whether to render with pygame')
-    # parser.add_argument('--train', action='store_true', default=False, help='Whether to train agents')
-    parser.add_argument('--train', action='store_true', default=True, help='Whether to train agents')
-    parser.add_argument('--max-steps', type=int, default=int(1e5), help='Total steps for training')
-    parser.add_argument('--episode-horizon', type=int, default=60, help='Total steps for every agents')
-    parser.add_argument('--save-interval', type=int, default=1, help='Number of steps between saving events')
-    parser.add_argument('--learning-rate', type=float, default=5e-4, help='Learning rate')
-    parser.add_argument('--gamma', type=float, default=0.99, help='discounting factor')
-    parser.add_argument('--log-dir', type=str, default='./logs', help='Logs output directory')
-    args = parser.parse_args()
-    
-    main()
+    app.run(main)
