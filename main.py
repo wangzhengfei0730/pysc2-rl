8e13617f360aca79b5974c474f0a0245edb3723e
wangzhengfei0730 <wangzhengfei@pku.edu.cn>
2019-02-23 09:39:10 +0800

    fix absl.flags bug

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ LOCK = threading.Lock()
 STEP = 0
 FLAGS = flags.FLAGS
 flags.DEFINE_string('map_name', 'MoveToBeacon', 'SC2LE mini-games map name')
-flags.DEFINE_integer('num_envs', 2, 'Number of environments')
+flags.DEFINE_integer('num_envs', 1, 'Number of environments')
 flags.DEFINE_integer('screen_resolution', 64, 'Resolution of the screen')
 flags.DEFINE_integer('minimap_resolution', 64, 'Resolution of the minimap')
 flags.DEFINE_integer('step_mul', 8, 'Number of game steps per agent step')
@@ -24,7 +24,7 @@ flags.DEFINE_integer('episode_horizon', 60, 'Total steps for every agents')
 flags.DEFINE_integer('save_interval', 10, 'Number of steps between saving events')
 flags.DEFINE_float('learning_rate', 5e-4, 'Learning rate')
 flags.DEFINE_float('gamma', 0.99, 'Discounting factor')
-flags.DEFINE_string('log_dir', './logs', 'Logs output directory')
+flags.DEFINE_string('logdir', './logs', 'Logs output directory')
 FLAGS(sys.argv)
 
 
@@ -40,7 +40,6 @@ def run_thread(agent, visualize):
         step_mul=FLAGS.step_mul,
         visualize=visualize
     ) as env:
-        env = available_actions_printer.AvailableActionsPrinter(env)
 
         replay_buffer = []
         for trajectory, done in rollout([agent], env, FLAGS.max_steps):
@@ -60,7 +59,7 @@ def run_thread(agent, visualize):
                         break
 
 
-def main():
+def main(argv):
     agents = []
     for i in range(FLAGS.num_envs):
         agent = A3CAgent(FLAGS.screen_resolution, FLAGS.minimap_resolution, FLAGS.train)
@@ -70,7 +69,7 @@ def main():
     config = tf.ConfigProto(allow_soft_placement=True)
     sess = tf.Session(config=config)
 
-    summary_writer = tf.summary.FileWriter(FLAGS.log_dir)
+    summary_writer = tf.summary.FileWriter(FLAGS.logdir)
     for i in range(FLAGS.num_envs):
         agents[i].setup(sess, summary_writer)
 
